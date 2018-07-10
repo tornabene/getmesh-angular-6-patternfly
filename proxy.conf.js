e02571f51917b8947a82aa433533766a12ef3441
tindaro tornabene <tindaro.tornabene@gmail.com>
2018-07-11 01:00:40 +0200

    modified:   proxy.conf.js

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,8 @@
 // This assumes that Mesh is running under localhost:8080
 module.exports = {
   "/api/*": {
-    target: "http://localhost:8071",
+  	target: "http://localhost:8070",
+  //  target: "http://www.laboratoriocloud.com:8070/",
     secure: false,
     changeOrigin: true
   }
